30fd2a70820bf4c5c1c351c3f667a0a750614f0f
Cole Wilson <cpwilson@ucsb.edu>
2026-04-28 22:12:33 -0700

    rwlock

--- rwlock.cc
+++ rwlock.cc
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include <pthread.h>
 #include <iostream> 
 
 
@@ -7,9 +6,54 @@
 
 //Your solution to implement each of the following methods
 //
-RWLock::RWLock() { }
-RWLock::~RWLock() { }
-void RWLock::startRead() { }
-void RWLock::doneRead() { }
-void RWLock::startWrite() { }
-void RWLock::doneWrite() { }
+RWLock::RWLock() : active_readers(0), waiting_writers(0), active_writer(false) {
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&readers_ok, NULL);
+    pthread_cond_init(&writers_ok, NULL);
+}
+
+RWLock::~RWLock() {
+    pthread_mutex_destroy(&mutex);
+    pthread_cond_destroy(&readers_ok);
+    pthread_cond_destroy(&writers_ok);
+}
+
+void RWLock::startRead() {
+    pthread_mutex_lock(&mutex);
+    while (active_writer || waiting_writers > 0) {
+        pthread_cond_wait(&readers_ok, &mutex);
+    }
+    ++active_readers;
+    pthread_mutex_unlock(&mutex);
+}
+
+void RWLock::doneRead() {
+    pthread_mutex_lock(&mutex);
+    --active_readers;
+    if (active_readers == 0) {
+        pthread_cond_signal(&writers_ok);
+    }
+    pthread_mutex_unlock(&mutex);
+}
+
+void RWLock::startWrite() {
+    pthread_mutex_lock(&mutex);
+    ++waiting_writers;
+    while (active_writer || active_readers > 0) {
+        pthread_cond_wait(&writers_ok, &mutex);
+    }
+    --waiting_writers;
+    active_writer = true;
+    pthread_mutex_unlock(&mutex);
+}
+
+void RWLock::doneWrite() {
+    pthread_mutex_lock(&mutex);
+    active_writer = false;
+    if (waiting_writers > 0) {
+        pthread_cond_signal(&writers_ok);
+    } else {
+        pthread_cond_broadcast(&readers_ok);
+    }
+    pthread_mutex_unlock(&mutex);
+}
--- rwlock.h
+++ rwlock.h
@@ -1,10 +1,18 @@
 #ifndef __RWLOCK_H__
 #define __RWLOCK_H__
-#include<semaphore.h>
+#include <semaphore.h>
+#include <pthread.h>
 
 class RWLock{
 private:
     //Your solution to add more private fields
+    pthread_mutex_t mutex;
+    pthread_cond_t readers_ok;
+    pthread_cond_t writers_ok;
+
+    int active_readers;
+    int waiting_writers;
+    bool active_writer;
     
 public:
     RWLock();
